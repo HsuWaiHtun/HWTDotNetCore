df122bb541cc89664f514395b4559e37c332f2c5
Hsu Wai Tun <hsuwaitun1103@gmail.com>
2024-05-14 10:34:57 +0630

    [Complete]JSon Data to API

---
+++ HWTDotNetCore.RestAPI/Db/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using HWTDotNetCore.RestAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HWTDotNetCore.RestAPI.Db;
+
+internal class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);//use sql server
+    }//what connection do you use
+    public DbSet<BlogModel> Blogs { get; set; } // ဘယ်tableကိုသုံးမှာလဲကြော်ငြာပေးရ(connect C# and database)
+}
